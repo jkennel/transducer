e2720753604ecd9c34d162a751407e1b2b8ce460
jkennel <jkennel@uoguelph.ca>
2019-08-27 09:12:50 -0400

    unnest function

---
+++ R/unnest_data.R
@@ -0,0 +1,14 @@
+#' unnest_data
+#' This function pulls the data and auxilary columns out of a nested data.table
+#'
+#' @param x the nested data.table of values
+#' @param columns the column names to include with the data
+#'
+#' @return
+#' @export
+#'
+unnest_data <- function(x, columns = c('serial', 'type', 'is_baro')) {
+
+  x[, data[[1]], by = mget(columns)]
+
+}
